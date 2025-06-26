2245a9dfb3388ec3e904233d043050d69891648c
kevin <stivekevinwatatyondep@gmail.com>
2025-06-27 00:26:00 +0200

    deploy-commit5-

--- skillmapp/urls.py
+++ skillmapp/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import TemplateView # Pour la vue de la page d'accueil sur render
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', TemplateView.as_view(template_name='login.html'), name='home'),  # Nouvelle route
     #----------------urls de l'application Comptes--------------------#
     path('comptes/', include('comptes.urls')),
     path('matching/', include('matching.urls')),
